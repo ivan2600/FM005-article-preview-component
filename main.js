f943c327bd37ff76ed64b2e89c83bfba4ced5794
Ivan Adamo <ivan.adamo.8@gmail.com>
2023-12-26 18:53:19 -0300

    challenge finished

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log('conectó');
+const shareButton = document.querySelector('.share-button');
+const shareSection = document.querySelector('.share-section');
+const avatarSection = document.querySelector('.avatar-section');
+
+shareButton.addEventListener('click', ()=> {
+  if (shareSection.classList.contains('activo')) {
+    // Borrar la clase activo
+    shareSection.classList.remove('activo')
+  } else {
+    // Si no la contiene, agregarla.
+    shareSection.classList.add('activo')
+  }
+  if (avatarSection.classList.contains('activo')) {
+    // Borrar la clase activo
+    avatarSection.classList.remove('activo')
+  } else {
+    // Si no la contiene, agregarla.
+    avatarSection.classList.add('activo')
+  }
+})
